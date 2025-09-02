49a4454c4cefad49fee3aebc2780cd28e4db1b9a
SkoVia <Covo4ek42@gmaail.com>
2025-09-02 23:29:59 +0700

    Windows_Starting_File

--- Comp&Link.bat
+++ Comp&Link.bat
@@ -3,4 +3,5 @@ gcc -c -O3 -march=native -g -Wall -Wextra SudokuLogic.c
 gcc -c -O3 -march=native -g -Wall -Wextra SudokuMain.c
 gcc -c -O3 -march=native -g -Wall -Wextra outputFuncs.c
 
+
 gcc -O3 -march=native -flto ChangesStack.o SudokuMain.o SudokuLogic.o outputFuncs.o -o sudoku
